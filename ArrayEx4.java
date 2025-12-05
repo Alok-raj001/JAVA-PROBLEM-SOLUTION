19a002363be2f2315d0d567507bdd7892cb45eb2
Alok Raj <67600651+Alok-raj001@users.noreply.github.com>
2025-12-05 19:14:02 +0530

    Add files via upload

---
+++ ArrayEx4.java
@@ -0,0 +1,19 @@
+
+public class ArrayEx4 {
+	public static void main(String[] args) {
+		int a[]=new int[] {6,8,5,4};
+		for (int i : a) {
+			System.out.println(i);
+		}
+		
+		double b[]=new double[] {2.3,5,.8,7.1};
+		for (double i : b) {
+			System.out.println(i);
+		}
+		
+		String c[]=new String[] {"Hello","Hi","Ok"};
+		for (String i : c) {
+			System.out.println(i);
+		}
+	}
+}
