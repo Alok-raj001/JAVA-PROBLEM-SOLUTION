382e93e3f342ca1f7cefa135cc56116ce19ffaab
Alok Raj <67600651+Alok-raj001@users.noreply.github.com>
2025-12-06 19:06:23 +0530

    Add files via upload

---
+++ ConstructorChainingEx.java
@@ -0,0 +1,32 @@
+class Human{
+	String name;
+	int age;
+	String gender;
+	String fname;
+	String mname;
+	double height;
+	double weight;
+	void show() {
+		System.out.println(name+" "+age+" "+gender+" "+fname+" "+mname+" "+height+" "+weight);
+	}
+	Human(String n,int a,String g){
+		name=n;
+		age=a;
+		gender=g;
+	}
+	Human(String n,int a,String g,String fn,String mn,double h,double w){
+		this(n,a,g);
+		fname=fn;
+		mname=mn;
+		height=h;
+		weight=w;
+	}
+}
+public class ConstructorChainingEx {
+	public static void main(String[] args) {
+		Human h1=new Human("Gopal Singh",21,"male","Bhopal Singh","Gopi Devi",5.6,55.3);
+		Human h2=new Human("Gopal Singh",21,"male");
+		h1.show();
+		h2.show();
+	}
+}
